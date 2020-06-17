46323d62d02ec6b506f942386f3ab370747269e3
Ramananda Panda <ramanandapanda@hotmail.com>
2020-06-17 20:04:25 +0530

    added port for env

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,7 @@
 module.exports = {
     "type": "postgres",
     "host": process.env.DATABASE_URL,
+    "port": process.env.DATABASE_PORT,
     "username": process.env.DATABASE_USER,
     "password": process.env.DATABASE_PASSWORD,
     "database": process.env.DATABASE_NAME,
