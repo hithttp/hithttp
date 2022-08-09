07cc037d1d58c88862405b97b8fa2250a1619315
Ramananda <ramanandapanda@hotmail.com>
2022-08-09 18:16:02 +0530

    script attr src CSP

--- src/main.ts
+++ src/main.ts
@@ -76,6 +76,7 @@ async function bootstrap() {
   app.use(helmet.contentSecurityPolicy({
     directives: {
       "script-src": ["'self'", "'unsafe-inline'", 'https://www.google.com', 'https://www.gstatic.com','https://cdnjs.cloudflare.com','https://www.googletagmanager.com','https://www.google-analytics.com'],
+      "script-src-attr":["'self'", "'unsafe-inline'"],
       "img-src": ["'self'", 'https://www.gstatic.com'],
       "frame-src": ['https://www.google.com']
     },
